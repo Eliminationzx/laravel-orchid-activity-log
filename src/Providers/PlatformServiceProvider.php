b0ddfbcfb555a3b2efe6d133719c803d940e8361
Muru <timurdelimin@gmail.com>
2026-03-22 20:47:48 +0700

    fix: update PlatformServiceProvider to use OrchidHelpers namespace instead of third-party package

--- src/Providers/PlatformServiceProvider.php
+++ src/Providers/PlatformServiceProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace OrchidActivityLog\Providers;
 
 use App\Models\Activity;
-use Manzadey\LaravelOrchidHelpers\Services\PlatformPermissionService;
+use OrchidHelpers\Services\PlatformPermissionService;
 use Orchid\Platform\OrchidServiceProvider;
 
 class PlatformServiceProvider extends OrchidServiceProvider
